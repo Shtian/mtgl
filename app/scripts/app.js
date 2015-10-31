4fe6ee6dd3bd0f16ab193c93a4c3239f06ef696f
Stian Haga <hagastian@gmail.com>
2015-10-31 17:37:53 +0100

    Add collection route and mock up home page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,10 +24,10 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/collection/:collectionId', {
+        templateUrl: 'views/collection.html',
+        controller: 'CollectionCtrl',
+        controllerAs: 'collection'
       })
       .otherwise({
         redirectTo: '/'
---
+++ app/scripts/controllers/collection.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alexandriaApp.controller:CollectionCtrl
+ * @description
+ * # CollectionCtrl
+ * Controller of the alexandriaApp
+ */
+angular.module('alexandriaApp')
+  .controller('CollectionCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
