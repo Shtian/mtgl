46096f9aaf85db071fc62c0dc03c8f7376d8d7c3
Stian Haga <hagastian@gmail.com>
2015-10-31 16:35:23 +0100

    initial commit with heroku app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alexandriaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the alexandriaApp
+ */
+angular.module('alexandriaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
